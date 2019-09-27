47c7994bee2353b58d7c19656aac663fc108bcb6
sylvain <sylvain.durand@gmail.com>
2019-09-27 17:23:40 +0200

    Update Version to 10.0.1

---
+++ ToolVersion.h
@@ -0,0 +1,26 @@
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
+// Copyright © 2011-2019 ANSSI. All Rights Reserved.
+//
+// Author(s): Jean Gautier (ANSSI)
+//
+
+#ifndef VERSION
+#    define VERSION 10, 0, 1, 000
+#endif
+#ifndef STR_VERSION
+#    define STR_VERSION "10.0.1.000"
+#endif
+#ifndef WSTR_VERSION
+#    define WSTR_VERSION L"10.0.1.000"
+#endif
+
+#define FILEVER VERSION
+#define PRODUCTVER VERSION
+#define STRFILEVER STR_VERSION
+#define STRPRODUCTVER STR_VERSION
+#define WSTRFILEVER WSTR_VERSION
+#define WSTRPRODUCTVER WSTR_VERSION
+
+#define WSTRPRODUCTNAME L"DFIR-ORC"
