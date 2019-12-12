52f0a8b375576cd5246503302c1f5bfe2d2ffaa3
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2019-12-12 16:17:22 +0100

    OrcCommand: ToolVersion: fill version with git tag

---
+++ src/OrcCommand/ToolVersion.cpp
@@ -0,0 +1,11 @@
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
+// Copyright © 2011-2019 ANSSI. All Rights Reserved.
+//
+// Author(s): fabienfl
+//
+
+#include "stdafx.h"
+
+#include "ToolVersionRes.h"
---
+++ src/OrcCommand/ToolVersion.h
@@ -0,0 +1,17 @@
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
+// Copyright © 2011-2019 ANSSI. All Rights Reserved.
+//
+// Author(s): fabienfl
+//
+
+//
+// Prefer including 'ToolVersion.h'
+//
+// This template will be fill by cmake.
+// It must not be included directly except from resource files as it would
+// trigger useless compilations.
+//
+
+#include "ToolVersionRes.h"
