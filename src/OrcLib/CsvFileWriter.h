4ca3eddd284acb4a303f5af0dcee4f40767a344e
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2020-01-07 10:20:01 +0100

    OrcLib: CsvFileWriter: Unescaped double quote characters in csv files
    https://github.com/DFIR-ORC/dfir-orc#13
    orc-windows/dfir-orc#24

--- src/OrcLib/CsvFileWriter.h
+++ src/OrcLib/CsvFileWriter.h
@@ -251,10 +251,53 @@ private:
 
     Writer(logger pLog, std::unique_ptr<Options>&& options);
 
+    //
+    // Workaround: 'Unescaped double quote characters in csv files #13 (github)'
+    //
+    // Double all quotes for escaping with CSV format but ignore first and last
+    // ones.
+    //
+    // A more complete approach is possible by specializing fmt::formatter but
+    // it could be less cpu friendly.
+    //
+    template <typename T>
+    struct EscapeQuoteInserter
+    {
+        using value_type = typename T::value_type;
+
+        EscapeQuoteInserter(T& wrapped)
+            : m_wrapped(wrapped)
+            , quoteCount(0)
+            , previousWasQuote(false)
+        {
+        }
+
+        template <typename U>
+        void push_back(U u)
+        {
+            if (previousWasQuote)
+            {
+                quoteCount++;
+
+                if (quoteCount > 1)
+                {
+                    m_wrapped.push_back('"');
+                }
+            }
+
+            previousWasQuote = (u == '"');
+
+            m_wrapped.push_back(u);
+        }
+
+        T& m_wrapped;
+        bool previousWasQuote;
+        size_t quoteCount;
+    };
+
     template <typename... Args>
     HRESULT FormatToBuffer(const std::wstring_view& strFormat, Args&&... args)
     {
-
         using char_type = WCHAR;
         using buffer_type = Buffer<char_type>;
         using wformat_iterator = std::back_insert_iterator<buffer_type>;
@@ -268,8 +311,14 @@ private:
 
         try
         {
-
-            auto result = fmt::format_to(std::back_inserter(buffer), strFormat, std::forward<Args>(args)...);
+            if (strFormat.find(L"\"{}\"") != std::wstring::npos)
+            {
+                fmt::format_to(std::back_inserter(EscapeQuoteInserter(buffer)), strFormat, args...);
+            }
+            else
+            {
+                fmt::format_to(std::back_inserter(buffer), strFormat, args...);
+            }
 
             if (!buffer.is_view())
             {
