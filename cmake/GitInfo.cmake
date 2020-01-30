6a0dce872c5a3eff7fb108458a5b44b7fe54eee1
fabienfl <fabienfl-orc@ssi.gouv.fr>
2020-01-30 12:41:10 +0100

    cmake: GitInfo: parse semver string to define major, minor, patch

--- cmake/GitInfo.cmake
+++ cmake/GitInfo.cmake
@@ -93,11 +93,50 @@ function(git_info)
             COMMAND
                 ${GIT_EXECUTABLE} describe --match "*[0-9].[0-9].[0-9]*" --tags --always
             WORKING_DIRECTORY ${_GIT_REPO_DIRECTORY}
-            OUTPUT_VARIABLE GIT_VERSION_FROM_SEMVER_TAG
+            OUTPUT_VARIABLE GIT_DESCRIBE
             OUTPUT_STRIP_TRAILING_WHITESPACE
         )
 
-        set(${_GIT_VERSION_FROM_SEMVER_TAG} ${GIT_VERSION_FROM_SEMVER_TAG} PARENT_SCOPE)
+        string(REGEX MATCH
+            "([0-9]+)\\.([0-9]+)\\.([0-9]+)-([0-9]+)-g([0-9a-f]+)"
+            VERSION_STRING
+            "${GIT_DESCRIBE}"
+        )
+
+        set(MAJOR 0)
+        set(MINOR 0)
+        set(PATCH 0)
+        set(DISTANCE 0)
+        set(COMMIT "")
+
+        if(CMAKE_MATCH_COUNT)
+            set(MAJOR ${CMAKE_MATCH_1})
+            set(MINOR ${CMAKE_MATCH_2})
+            set(PATCH ${CMAKE_MATCH_3})
+            set(DISTANCE ${CMAKE_MATCH_4})
+            set(COMMIT ${CMAKE_MATCH_5})
+        else()
+            string(REGEX MATCH
+                "([0-9]+)\\.([0-9]+)\\.([0-9]+)"
+                VERSION_STRING
+                "${GIT_DESCRIBE}"
+            )
+
+            if(CMAKE_MATCH_COUNT)
+                set(MAJOR ${CMAKE_MATCH_1})
+                set(MINOR ${CMAKE_MATCH_2})
+                set(PATCH ${CMAKE_MATCH_3})
+            endif()
+        endif()
+
+        message(STATUS "Found SemVer: ${VERSION_STRING} (${GIT_DESCRIBE})")
+
+        set(${_GIT_VERSION_FROM_SEMVER_TAG} ${GIT_DESCRIBE} PARENT_SCOPE)
+        set(${_GIT_VERSION_FROM_SEMVER_TAG}_MAJOR ${MAJOR} PARENT_SCOPE)
+        set(${_GIT_VERSION_FROM_SEMVER_TAG}_MINOR ${MINOR} PARENT_SCOPE)
+        set(${_GIT_VERSION_FROM_SEMVER_TAG}_PATCH ${PATCH} PARENT_SCOPE)
+        set(${_GIT_VERSION_FROM_SEMVER_TAG}_DISTANCE ${DISTANCE} PARENT_SCOPE)
+        set(${_GIT_VERSION_FROM_SEMVER_TAG}_COMMIT ${COMMIT} PARENT_SCOPE)
     endif()
 
     # Current branch
