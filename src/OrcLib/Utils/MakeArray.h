cf9040894ef46311d00bcb7755cfaaf6f5d01541
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2020-09-29 20:18:40 +0200

    OrcLib: Utils: add MakeArray

---
+++ src/OrcLib/Utils/MakeArray.h
@@ -0,0 +1,24 @@
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
+// Copyright © 2020 ANSSI. All Rights Reserved.
+//
+// Author(s): fabienfl (ANSSI)
+//
+#pragma once
+
+#include <array>
+
+namespace Orc {
+
+//
+// Helper for making std::array without specifying the size
+// https://stackoverflow.com/a/26351760
+//
+template <typename V, typename... T>
+constexpr auto MakeArray(T&&... t) -> std::array<V, sizeof...(T)>
+{
+    return {{std::forward<T>(t)...}};
+}
+
+}  // namespace Orc
