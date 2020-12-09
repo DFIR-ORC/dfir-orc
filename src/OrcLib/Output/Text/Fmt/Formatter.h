20538188233d1d809c4785872b5ad18482dadf4d
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2020-12-09 18:25:05 +0100

    OrcLib: Text: Fmt: add specialization for Result

--- src/OrcLib/Output/Text/Fmt/Formatter.h
+++ src/OrcLib/Output/Text/Fmt/Formatter.h
@@ -25,4 +25,5 @@
 #include "Output/Text/Fmt/Fwd/PartitionType.h"
 #include "Output/Text/Fmt/Fwd/PartitionFlags.h"
 #include "Output/Text/Fmt/Fwd/SYSTEMTIME.h"
+#include "Output/Text/Fmt/Fwd/Result.h"
 #include "Output/Text/Fmt/Fwd/TimeUtc.h"
