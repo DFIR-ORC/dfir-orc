f925e31f7b88f7db931cb6e1b44c939acae51fca
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2021-01-25 19:30:40 +0100

    OrcLib: add enum class FileDisposition

---
+++ src/OrcLib/FileDisposition.h
@@ -0,0 +1,30 @@
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
+// Copyright © 2021 ANSSI. All Rights Reserved.
+//
+// Author(s): fabienfl
+//
+
+#pragma once
+
+#include <string_view>
+#include <system_error>
+#include <ios>
+
+namespace Orc {
+
+enum class FileDisposition
+{
+    Unknown,
+    Append,
+    Truncate,
+    CreateNew
+};
+
+std::wstring_view ToString(FileDisposition disposition);
+FileDisposition ToFileDisposition(const std::wstring& disposition, std::error_code& ec = std::error_code());
+
+std::ios_base::openmode ToOpenMode(FileDisposition disposition);
+
+}  // namespace Orc
