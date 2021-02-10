dc186eded9adbc07a82473114541f6896e93d8d8
Lionel <lionel.prat9@gmail.com>
2021-02-10 13:49:37 +0100

    GetSamples: enable autorunsc for all users

--- src/OrcCommand/Command/GetSamples/GetSamples_Run.cpp
+++ src/OrcCommand/Command/GetSamples/GetSamples_Run.cpp
@@ -77,7 +77,7 @@ HRESULT Main::LoadAutoRuns(TaskTracker& tk, LPCWSTR szTempDir)
     command->AddExecutableToRun(strAutorunsCmd);
     command->AddOnCompleteAction(
         std::make_shared<OnComplete>(OnComplete::Delete, L"autorunsc.exe", strAutorunsCmd, nullptr));
-    command->AddArgument(L"/accepteula -t -h -x -a *", 0L);
+    command->AddArgument(L"/accepteula -t -h -x -a * *", 0L);
 
     auto pMemStream = std::make_shared<MemoryStream>();
     hr = pMemStream->OpenForReadWrite();
