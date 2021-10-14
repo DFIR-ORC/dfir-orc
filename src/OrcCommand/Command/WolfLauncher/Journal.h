7ce7c9648d01cf6148358f18218e8068e002a20c
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2021-10-14 18:05:26 +0200

    OrcCommand: WolfLauncher: Journal: use syslog's timestamp

--- src/OrcCommand/Command/WolfLauncher/Journal.h
+++ src/OrcCommand/Command/WolfLauncher/Journal.h
@@ -18,6 +18,7 @@
 #include "Console.h"
 
 #include "UploadNotification.h"
+#include "Text/Iconv.h"
 
 namespace Orc::Command::Wolf {
 
@@ -33,21 +34,23 @@ public:
     template <typename... FmtArgs>
     void Print(const std::wstring_view& commandSet, const std::wstring_view& agent, FmtArgs&&... status) const
     {
+        const auto timepoint = std::chrono::system_clock::now();
+
         std::wstring message;
         Text::FormatToWithoutEOL(std::back_inserter(message), "{:<16} {:<26} ", commandSet, agent);
         Text::FormatToWithoutEOL(std::back_inserter(message), std::forward<FmtArgs>(status)...);
 
         // TODO: instead of using console directly the journal facility could have a custom console sink
+        std::time_t now = std::chrono::system_clock::to_time_t(timepoint);
         {
             std::lock_guard lock(m_mutex);
-            std::time_t now = std::time(nullptr);
             m_console.Print(L"{:%Y-%m-%dT%H:%M:%SZ}   {}", fmt::gmtime(now), message);
         }
 
         const auto& journal = Orc::Log::DefaultLogger()->Get(Log::Facility::kJournal);
         if (journal)
         {
-            journal->Info(L"{}", message);
+            journal->Log(timepoint, Log::Level::Info, Utf16ToUtf8(message, "<unicode conversion failed>"));
         }
     }
 
