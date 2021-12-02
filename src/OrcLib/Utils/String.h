5ca5b3f67e720ad177c6391995e19bfb7a3a5ece
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2021-12-02 15:24:50 +0100

    OrcLib: Utils: String: add function StartsWith

---
+++ src/OrcLib/Utils/String.h
@@ -0,0 +1,17 @@
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
+// Copyright © 2021 ANSSI. All Rights Reserved.
+//
+// Author(s): fabienfl (ANSSI)
+//
+#pragma once
+
+#include <string_view>
+
+namespace Orc {
+
+bool StartsWith(std::string_view string, std::string_view substring);
+bool StartsWith(std::wstring_view string, std::wstring_view substring);
+
+}  // namespace Orc
