38ea7acb0a6da41fa401b6d5d1b696284807ac63
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2022-03-15 15:48:23 +0100

    OrcLib: Utils: String: add function Join

--- src/OrcLib/Utils/String.h
+++ src/OrcLib/Utils/String.h
@@ -17,4 +17,48 @@ bool StartsWith(std::wstring_view string, std::wstring_view substring);
 bool EndsWith(std::string_view string, std::string_view substring);
 bool EndsWith(std::wstring_view string, std::wstring_view substring);
 
+template <typename InputIt, typename CharT>
+void Join(InputIt begin, InputIt end, std::basic_string<CharT>& output, CharT delimiter)
+{
+    const auto itemCount = std::distance(begin, end);
+
+    size_t charCount = 0;
+    for (auto it = begin; it != end; ++it)
+    {
+        charCount += (*it).size();
+    }
+
+    if (charCount == 0)
+    {
+        return;
+    }
+
+    output.reserve(charCount + itemCount - 1);
+    Join(begin, end, std::back_inserter(output), delimiter);
+}
+
+template <typename InputIt, typename OutputIt, typename CharT>
+void Join(InputIt begin, InputIt end, OutputIt outputIt, CharT delimiter)
+{
+    const auto itemCount = std::distance(begin, end);
+
+    if (begin == end)
+    {
+        return;
+    }
+
+    std::copy(std::cbegin(*begin), std::cend(*begin), outputIt);
+    begin++;
+
+    size_t i = 1;
+    for (auto it = begin; it != end; ++it)
+    {
+        *outputIt = delimiter;
+        outputIt++;
+
+        const auto& value = *it;
+        std::copy(std::cbegin(value), std::cend(value), outputIt);
+    }
+}
+
 }  // namespace Orc
