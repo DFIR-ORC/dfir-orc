c6e5b8b205ff7a74ecb248be9540be11610e0d6e
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2022-05-05 13:04:41 +0200

    OrcLib: Utils: BufferView: add ToBufferView conversion overloads

--- src/OrcLib/Utils/BufferView.h
+++ src/OrcLib/Utils/BufferView.h
@@ -18,10 +18,50 @@ namespace Orc {
 template <typename T>
 using BasicBufferView = gsl::span<const T>;
 
+//
+// BasicBufferView
+//
+
 using BufferView = BasicBufferView<uint8_t>;
 
+inline BasicBufferView<char> ToBasicBufferView(const char* szBuffer)
+{
+    return BasicBufferView<char>(szBuffer, strlen(szBuffer));
+}
+
+inline BasicBufferView<wchar_t> ToBasicBufferView(const wchar_t* szBuffer)
+{
+    return BasicBufferView<wchar_t>(szBuffer, wcslen(szBuffer));
+}
+
+template <typename T>
+inline BasicBufferView<typename T::value_type> ToBasicBufferView(const T& buffer)
+{
+    return BasicBufferView<typename T::value_type>(buffer.data(), buffer.size());
+}
+
+//
+// BufferView
+//
+
+inline BufferView ToBufferView(const char* buffer)
+{
+    return BufferView(reinterpret_cast<const uint8_t*>(buffer), strlen(buffer));
+}
+
+inline BufferView ToBufferView(const wchar_t* buffer)
+{
+    return BufferView(reinterpret_cast<const uint8_t*>(buffer), wcslen(buffer) * sizeof(wchar_t));
+}
+
+template <typename T>
+inline BufferView ToBufferView(const T& buffer)
+{
+    return BufferView(reinterpret_cast<const uint8_t*>(buffer.data()), buffer.size() * sizeof(T::value_type));
+}
+
 template <typename T = uint8_t, typename ContainerT>
-BasicBufferView<T> MakeSubView(const ContainerT& container, uint64_t offset, std::error_code& ec)
+inline BasicBufferView<T> MakeSubView(const ContainerT& container, uint64_t offset, std::error_code& ec)
 {
     if (container.size() < offset)
     {
@@ -33,7 +73,7 @@ BasicBufferView<T> MakeSubView(const ContainerT& container, uint64_t offset, std
 }
 
 template <typename T = uint8_t, typename ContainerT>
-BasicBufferView<T> MakeSubView(const ContainerT& container, uint64_t offset, size_t count, std::error_code& ec)
+inline BasicBufferView<T> MakeSubView(const ContainerT& container, uint64_t offset, size_t count, std::error_code& ec)
 {
     if (container.size() < offset || container.size() - offset < count * sizeof(T))
     {
