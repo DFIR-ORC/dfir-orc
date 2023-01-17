9455bf28c76ac13ef74debc0b0f98b5bc204df1a
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2023-01-17 17:14:05 +0100

    OrcLib: Log: FileSink: increase first logs buffer size to 128k

--- src/OrcLib/Log/FileSink.h
+++ src/OrcLib/Log/FileSink.h
@@ -38,7 +38,7 @@ public:
     using SpdlogFileSink = spdlog::sinks::basic_file_sink_st;
     using MemorySink = MemorySink<std::vector<uint8_t>, spdlog::details::null_mutex>;
 
-    const size_t kMemorySinkSize = 16384;
+    const size_t kMemorySinkSize = 128000;
 
     FileSink()
         : m_fileSink()
