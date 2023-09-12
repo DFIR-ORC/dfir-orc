12f62bc33cafd15077a57fa6ffcb2c1126ce6489
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2023-09-12 16:20:21 +0200

    OrcLib: Filesystem: add FileAttribute

---
+++ src/OrcLib/Filesystem/FileAttribute.cpp
@@ -0,0 +1,226 @@
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
+// Copyright © 2023 ANSSI. All Rights Reserved.
+//
+// Author(s): fabienfl (ANSSI)
+//
+#include "FileAttribute.h"
+
+#include <array>
+#include <tuple>
+
+#include "Log/Log.h"
+#include "Utils/String.h"
+
+using namespace Orc;
+
+namespace {
+
+constexpr std::string_view kUnknown = "<unknown>";
+constexpr std::string_view kNone = "<none>";
+
+constexpr std::string_view kFileAttributeReadOnly = "FILE_ATTRIBUTE_READONLY";
+constexpr std::string_view kFileAttributeHidden = "FILE_ATTRIBUTE_HIDDEN";
+constexpr std::string_view kFileAttributeSystem = "FILE_ATTRIBUTE_SYSTEM";
+constexpr std::string_view kFileAttributeDirectory = "FILE_ATTRIBUTE_DIRECTORY";
+constexpr std::string_view kFileAttributeArchive = "FILE_ATTRIBUTE_ARCHIVE";
+constexpr std::string_view kFileAttributeDevice = "FILE_ATTRIBUTE_DEVICE";
+constexpr std::string_view kFileAttributeNormal = "FILE_ATTRIBUTE_NORMAL";
+constexpr std::string_view kFileAttributeTemporary = "FILE_ATTRIBUTE_TEMPORARY";
+constexpr std::string_view kFileAttributeSparseFile = "FILE_ATTRIBUTE_SPARSE_FILE";
+constexpr std::string_view kFileAttributeReparsePoint = "FILE_ATTRIBUTE_REPARSE_POINT";
+constexpr std::string_view kFileAttributeCompressed = "FILE_ATTRIBUTE_COMPRESSED";
+constexpr std::string_view kFileAttributeOffline = "FILE_ATTRIBUTE_OFFLINE";
+constexpr std::string_view kFileAttributeNotContentIndexed = "FILE_ATTRIBUTE_NOT_CONTENT_INDEXED";
+constexpr std::string_view kFileAttributeEncrypted = "FILE_ATTRIBUTE_ENCRYPTED";
+constexpr std::string_view kFileAttributeIntegrityStream = "FILE_ATTRIBUTE_INTEGRITY_STREAM";
+constexpr std::string_view kFileAttributeVirtual = "FILE_ATTRIBUTE_VIRTUAL";
+constexpr std::string_view kFileAttributeNoScrubData = "FILE_ATTRIBUTE_NO_SCRUB_DATA";
+constexpr std::string_view kFileAttributeEA = "FILE_ATTRIBUTE_EA";
+constexpr std::string_view kFileAttributePinned = "FILE_ATTRIBUTE_PINNED";
+constexpr std::string_view kFileAttributeUnpinned = "FILE_ATTRIBUTE_UNPINNED";
+constexpr std::string_view kFileAttributeRecallOnOpen = "FILE_ATTRIBUTE_RECALL_ON_OPEN";
+constexpr std::string_view kFileAttributeRecallOnDataAccess = "FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS";
+
+constexpr std::array kFileAttributes = {
+    std::tuple {FileAttribute::kFileAttributeReadOnly, kFileAttributeReadOnly, 'R'},
+    std::tuple {FileAttribute::kFileAttributeHidden, kFileAttributeHidden, 'H'},
+    std::tuple {FileAttribute::kFileAttributeSystem, kFileAttributeSystem, 'S'},
+    std::tuple {FileAttribute::kFileAttributeDirectory, kFileAttributeDirectory, 'D'},
+    std::tuple {FileAttribute::kFileAttributeArchive, kFileAttributeArchive, 'A'},
+    std::tuple {FileAttribute::kFileAttributeDevice, kFileAttributeDevice, 'd'},
+    std::tuple {FileAttribute::kFileAttributeNormal, kFileAttributeNormal, 'N'},
+    std::tuple {FileAttribute::kFileAttributeTemporary, kFileAttributeTemporary, 'T'},
+    std::tuple {FileAttribute::kFileAttributeSparseFile, kFileAttributeSparseFile, 'P'},
+    std::tuple {FileAttribute::kFileAttributeReparsePoint, kFileAttributeReparsePoint, 'L'},
+    std::tuple {FileAttribute::kFileAttributeCompressed, kFileAttributeCompressed, 'C'},
+    std::tuple {FileAttribute::kFileAttributeOffline, kFileAttributeOffline, 'O'},
+    std::tuple {FileAttribute::kFileAttributeNotContentIndexed, kFileAttributeNotContentIndexed, 'I'},
+    std::tuple {FileAttribute::kFileAttributeEncrypted, kFileAttributeEncrypted, 'E'},
+    std::tuple {FileAttribute::kFileAttributeIntegrityStream, kFileAttributeIntegrityStream, 's'},
+    std::tuple {FileAttribute::kFileAttributeVirtual, kFileAttributeVirtual, 'V'},
+    std::tuple {FileAttribute::kFileAttributeNoScrubData, kFileAttributeNoScrubData, 'B'},
+    std::tuple {FileAttribute::kFileAttributeEA, kFileAttributeEA, 'e'},
+    std::tuple {FileAttribute::kFileAttributePinned, kFileAttributePinned, 'p'},
+    std::tuple {FileAttribute::kFileAttributeUnpinned, kFileAttributeUnpinned, 'u'},
+    std::tuple {FileAttribute::kFileAttributeRecallOnOpen, kFileAttributeRecallOnOpen, 'o'},
+    std::tuple {FileAttribute::kFileAttributeRecallOnDataAccess, kFileAttributeRecallOnDataAccess, 'a'},
+};
+
+bool HasInvalidFlag(uint32_t flags)
+{
+    using namespace Orc;
+
+    auto remainingFlags = flags;
+    for (const auto& [flag, string, identifier] : kFileAttributes)
+    {
+        if (HasFlag(static_cast<FileAttribute>(flags), flag))
+        {
+            remainingFlags -= static_cast<std::underlying_type_t<FileAttribute>>(flag);
+        }
+    }
+
+    if (remainingFlags)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+}  // namespace
+
+namespace Orc {
+
+std::string ToString(FileAttribute flags)
+{
+    if (flags == static_cast<FileAttribute>(0))
+    {
+        return std::string(kNone);
+    }
+
+    if (::HasInvalidFlag(std::underlying_type_t<FileAttribute>(flags)))
+    {
+        Log::Debug(
+            "Failed to convert some FileAttribute flags to string (value: {})",
+            std::underlying_type_t<FileAttribute>(flags));
+
+        return fmt::format("{:#x}", std::underlying_type_t<FileAttribute>(flags));
+    }
+
+    std::vector<std::string_view> strings;
+    for (const auto& [flag, string, identifier] : kFileAttributes)
+    {
+        if (HasFlag(flags, flag))
+        {
+            strings.push_back(string);
+        }
+    }
+
+    std::string output;
+    Join(std::cbegin(strings), std::cend(strings), std::back_inserter(output), '|');
+    return output;
+}
+
+std::wstring ToStringW(FileAttribute flags)
+{
+    auto s = ToString(flags);
+
+    std::wstring output;
+    std::copy(std::cbegin(s), std::cend(s), std::back_inserter(output));
+    return output;
+}
+
+char ToIdentifier(FileAttribute flags)
+{
+    if (flags == static_cast<FileAttribute>(0))
+    {
+        return '?';
+    }
+
+    if (::HasInvalidFlag(std::underlying_type_t<FileAttribute>(flags)))
+    {
+        Log::Debug(
+            "Failed to convert some FileAttribute flags to identifier (value: {})",
+            std::underlying_type_t<FileAttribute>(flags));
+
+        return '?';
+    }
+
+    for (const auto& [f, s, identifier] : kFileAttributes)
+    {
+        if (HasFlag(flags, f))
+        {
+            if (flags != f)
+            {
+                Log::Debug(
+                    "Failed to convert some FileAttribute flags to unique identifier (value: {})",
+                    std::underlying_type_t<FileAttribute>(flags));
+                return '?';
+            }
+
+            return identifier;
+        }
+    }
+
+    return '?';
+}
+
+wchar_t ToIdentifierW(FileAttribute flags)
+{
+    return ToIdentifier(flags);
+}
+
+std::string ToIdentifiers(FileAttribute flags)
+{
+    if (flags == static_cast<FileAttribute>(0))
+    {
+        return std::string(kNone);
+    }
+
+    if (::HasInvalidFlag(std::underlying_type_t<FileAttribute>(flags)))
+    {
+        Log::Debug(
+            "Failed to convert some FileAttribute flags to string (value: {})",
+            std::underlying_type_t<FileAttribute>(flags));
+
+        return fmt::format("{:#x}", std::underlying_type_t<FileAttribute>(flags));
+    }
+
+    std::string output;
+    for (const auto& [flag, s, identifier] : kFileAttributes)
+    {
+        if (HasFlag(flags, flag))
+        {
+            output.push_back(identifier);
+        }
+        else
+        {
+            output.push_back('.');
+        }
+    }
+
+    return output;
+}
+
+std::wstring ToIdentifiersW(FileAttribute flags)
+{
+    auto s = ToIdentifiers(flags);
+
+    std::wstring output;
+    std::copy(std::cbegin(s), std::cend(s), std::back_inserter(output));
+    return output;
+}
+
+FileAttribute ToFileAttribute(const uint32_t attributes, std::error_code& ec)
+{
+    if (::HasInvalidFlag(std::underlying_type_t<FileAttribute>(attributes)))
+    {
+        ec = std::make_error_code(std::errc::invalid_argument);
+    }
+
+    return static_cast<FileAttribute>(attributes);
+}
+
+}  // namespace Orc
---
+++ src/OrcLib/Filesystem/FileAttribute.h
@@ -0,0 +1,85 @@
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
+// Copyright © 2023 ANSSI. All Rights Reserved.
+//
+// Author(s): fabienfl (ANSSI)
+//
+
+#include "Utils/EnumFlags.h"
+
+namespace Orc {
+
+// From 'winnt.h'
+#define ORC_FILE_ATTRIBUTE_READONLY 0x00000001
+#define ORC_FILE_ATTRIBUTE_HIDDEN 0x00000002
+#define ORC_FILE_ATTRIBUTE_SYSTEM 0x00000004
+#define ORC_FILE_ATTRIBUTE_DIRECTORY 0x00000010
+#define ORC_FILE_ATTRIBUTE_ARCHIVE 0x00000020
+#define ORC_FILE_ATTRIBUTE_DEVICE 0x00000040
+#define ORC_FILE_ATTRIBUTE_NORMAL 0x00000080
+#define ORC_FILE_ATTRIBUTE_TEMPORARY 0x00000100
+#define ORC_FILE_ATTRIBUTE_SPARSE_FILE 0x00000200
+#define ORC_FILE_ATTRIBUTE_REPARSE_POINT 0x00000400
+#define ORC_FILE_ATTRIBUTE_COMPRESSED 0x00000800
+#define ORC_FILE_ATTRIBUTE_OFFLINE 0x00001000
+#define ORC_FILE_ATTRIBUTE_NOT_CONTENT_INDEXED 0x00002000
+#define ORC_FILE_ATTRIBUTE_ENCRYPTED 0x00004000
+#define ORC_FILE_ATTRIBUTE_INTEGRITY_STREAM 0x00008000
+#define ORC_FILE_ATTRIBUTE_VIRTUAL 0x00010000
+#define ORC_FILE_ATTRIBUTE_NO_SCRUB_DATA 0x00020000
+#define ORC_FILE_ATTRIBUTE_EA 0x00040000
+#define ORC_FILE_ATTRIBUTE_PINNED 0x00080000
+#define ORC_FILE_ATTRIBUTE_UNPINNED 0x00100000
+#define ORC_FILE_ATTRIBUTE_RECALL_ON_OPEN 0x00040000
+#define ORC_FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS 0x00400000
+
+enum class FileAttribute
+{
+    kFileAttributeReadOnly = ORC_FILE_ATTRIBUTE_READONLY,
+    kFileAttributeHidden = ORC_FILE_ATTRIBUTE_HIDDEN,
+    kFileAttributeSystem = ORC_FILE_ATTRIBUTE_SYSTEM,
+    kFileAttributeDirectory = ORC_FILE_ATTRIBUTE_DIRECTORY,
+    kFileAttributeArchive = ORC_FILE_ATTRIBUTE_ARCHIVE,
+    kFileAttributeDevice = ORC_FILE_ATTRIBUTE_DEVICE,
+    kFileAttributeNormal = ORC_FILE_ATTRIBUTE_NORMAL,
+    kFileAttributeTemporary = ORC_FILE_ATTRIBUTE_TEMPORARY,
+    kFileAttributeSparseFile = ORC_FILE_ATTRIBUTE_SPARSE_FILE,
+    kFileAttributeReparsePoint = ORC_FILE_ATTRIBUTE_REPARSE_POINT,
+    kFileAttributeCompressed = ORC_FILE_ATTRIBUTE_COMPRESSED,
+    kFileAttributeOffline = ORC_FILE_ATTRIBUTE_OFFLINE,
+    kFileAttributeNotContentIndexed = ORC_FILE_ATTRIBUTE_NOT_CONTENT_INDEXED,
+    kFileAttributeEncrypted = ORC_FILE_ATTRIBUTE_ENCRYPTED,
+    kFileAttributeIntegrityStream = ORC_FILE_ATTRIBUTE_INTEGRITY_STREAM,
+    kFileAttributeVirtual = ORC_FILE_ATTRIBUTE_VIRTUAL,
+    kFileAttributeNoScrubData = ORC_FILE_ATTRIBUTE_NO_SCRUB_DATA,
+    kFileAttributeEA = ORC_FILE_ATTRIBUTE_EA,
+    kFileAttributePinned = ORC_FILE_ATTRIBUTE_PINNED,
+    kFileAttributeUnpinned = ORC_FILE_ATTRIBUTE_UNPINNED,
+    kFileAttributeRecallOnOpen = ORC_FILE_ATTRIBUTE_RECALL_ON_OPEN,
+    kFileAttributeRecallOnDataAccess = ORC_FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS
+};
+
+ENABLE_BITMASK_OPERATORS(FileAttribute)
+
+// Example: 0x880 => FILE_ATTRIBUTE_NORMAL|FILE_ATTRIBUTE_HIDDEN|FILE_ATTRIBUTE_SYSTEM
+std::string ToString(FileAttribute attributes);
+
+// Example: 0x880 => FILE_ATTRIBUTE_NORMAL|FILE_ATTRIBUTE_HIDDEN|FILE_ATTRIBUTE_SYSTEM
+std::wstring ToStringW(FileAttribute attributes);
+
+// Example: 0x880 => ....HN....S...........
+std::string ToIdentifiers(FileAttribute attributes);
+
+// Example: 0x880 => ....HN....S...........
+std::wstring ToIdentifiersW(FileAttribute attributes);
+
+// Example: 0x80 => N
+char ToIdentifier(FileAttribute flags);
+
+// Example: 0x80 => N
+wchar_t ToIdentifierW(FileAttribute flags);
+
+FileAttribute ToFileAttribute(const uint32_t attributes, std::error_code& ec);
+
+}  // namespace Orc
