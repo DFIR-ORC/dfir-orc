ff69f826c19bcb330ec27096c46891a33578e7c5
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2024-02-28 14:58:31 +0100

    OrcLib: Utils: Dump: replace fstream with win32 api

--- src/OrcLib/Utils/Dump.h
+++ src/OrcLib/Utils/Dump.h
@@ -8,16 +8,49 @@
 
 #pragma once
 
-#include <fstream>
 #include <filesystem>
+
+#include "Utils/Result.h"
+#include "Utils/Guard.h"
 
 namespace Orc {
 
 template <typename T>
-void Dump(const T& container, const std::filesystem::path& output)
+Result<void> Dump(const T& container, const std::filesystem::path& output)
 {
-    std::ofstream ofs(output, std::ios_base::binary);
-    std::copy(std::cbegin(container), std::cend(container), std::ostreambuf_iterator<char>(ofs));
+    using value_type = T::value_type;
+    std::string_view buffer(reinterpret_cast<const char*>(container.data()), container.size() * sizeof(T::value_type));
+
+    Guard::FileHandle file = CreateFileW(
+        output.c_str(),
+        GENERIC_WRITE,
+        FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+        NULL,
+        CREATE_NEW,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+    if (!file.IsValid())
+    {
+        auto ec = LastWin32Error();
+        Log::Debug("Failed CreateFile [{}]", ec);
+        return ec;
+    }
+
+    DWORD written = 0;
+    if (!WriteFile(file.value(), buffer.data(), static_cast<DWORD>(buffer.size()), &written, NULL))
+    {
+        auto ec = LastWin32Error();
+        Log::Debug("Failed WriteFile [{}]", ec);
+        return ec;
+    }
+
+    if (written != buffer.size())
+    {
+        Log::Debug("Partial write (expected: {}, done: {})", buffer.size(), written);
+        return std::make_error_code(std::errc::message_size);
+    }
+
+    return Orc::Success<>();
 }
 
 }  // namespace Orc
