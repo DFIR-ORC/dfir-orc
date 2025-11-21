c881b8ba1fb13f4ebb43ccc88ca29cda5e92298a
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2025-11-21 15:26:45 +0100

    OrcLib: add function EndsWithNoCase

--- src/OrcLib/Strings.h
+++ src/OrcLib/Strings.h
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <string>
+#include <cwctype>
 
 namespace Orc {
 
@@ -13,6 +14,15 @@ inline bool EndsWith(const std::wstring& string, const std::wstring& substring) 
     }
 
     return std::equal(substring.rbegin(), substring.rend(), string.rbegin());
+}
+
+inline bool EndsWithNoCase(const std::wstring& string, const std::wstring& substring) noexcept
+{
+    if (substring.size() > string.size())
+        return false;
+    return std::equal(substring.rbegin(), substring.rend(), string.rbegin(), [](wchar_t a, wchar_t b) {
+        return std::towlower(a) == std::towlower(b);
+    });
 }
 
 }  // namespace Orc
