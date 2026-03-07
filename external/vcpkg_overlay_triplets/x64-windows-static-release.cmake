e3502782f052eef7403996fbd323ca33c625ac98
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2026-03-07 11:43:20 +0100

    vcpkg: add vcpkg_overlay_triplets

---
+++ external/vcpkg_overlay_triplets/x64-windows-static-release.cmake
@@ -0,0 +1,2 @@
+include("${CMAKE_CURRENT_LIST_DIR}/x64-windows-static.cmake")
+set(VCPKG_BUILD_TYPE release)
---
+++ external/vcpkg_overlay_triplets/x86-windows-static-release.cmake
@@ -0,0 +1,2 @@
+include("${CMAKE_CURRENT_LIST_DIR}/x86-windows-static.cmake")
+set(VCPKG_BUILD_TYPE release)
