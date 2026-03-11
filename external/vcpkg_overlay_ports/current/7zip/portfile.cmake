9ccf87080f84bf97784ead345bdeed517a9c6176
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2026-03-11 22:25:44 +0100

    vcpkg: add vcpkg_overlay_ports/current for x86, x64 and arm64

---
+++ external/vcpkg_overlay_ports/current/7zip/portfile.cmake
@@ -0,0 +1,40 @@
+string(REGEX REPLACE "[.]([0-9])\$" ".0\\1" upstream_version "${VERSION}")
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO ip7z/7zip
+    REF "${upstream_version}"
+    SHA512 eb5ed600f82aca52f6dc8d2be3e4da4380670308dff2bcbfc96255d9d23bb5ca35dea073bd97070f0a1891b2f329d88a06b304f48e30f4ad89256c7664e9c1ea
+    HEAD_REF main
+    PATCHES
+        add-functions-and-fixes-for-static-link.patch
+        my-com.patch
+        sort-asm.diff
+        fix_timespec_get_broken_on_android.patch
+)
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/7zip-config.cmake.in" DESTINATION "${SOURCE_PATH}")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        "-DVCPKG_TARGET_ARCHITECTURE=${VCPKG_TARGET_ARCHITECTURE}"
+)
+
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+vcpkg_cmake_config_fixup()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/DOC/License.txt")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(
+    COPY
+        ${CMAKE_CURRENT_LIST_DIR}/7zip.h
+        ${CMAKE_CURRENT_LIST_DIR}/guids.h
+        ${CMAKE_CURRENT_LIST_DIR}/extras.h
+    DESTINATION ${CURRENT_PACKAGES_DIR}/include/7zip
+)
+
